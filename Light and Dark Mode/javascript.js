e49d576f940fe95f42b20accb45c269a81a7bfcf
Amila <121929892+AmilaRanasingheWork@users.noreply.github.com>
2025-02-11 20:33:37 +0200

    Add files via upload

---
+++ Light and Dark Mode/javascript.js
@@ -0,0 +1,28 @@
+function toggle(){
+    var input = document.getElementById("checkBox");
+    if(input.checked == true){
+        document.body.style.backgroundColor = "#343434";
+    } else{
+        document.body.style.backgroundColor = "white";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
